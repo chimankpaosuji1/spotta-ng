0edb296fb2cf914da321d8333304d9bd233583f7
Chimankpa Osuji <chimankpaosuji@gmail.com>
2024-04-08 23:18:07 +0100

    first commit

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="w-full  pt-[80px]">
+      <div className="bg-[#F2F6FD] pt-[20px]">
+        <div className=" w-[90%] mx-auto flex flex-col items-start justify-start ">
+          <div className="font-inter text-[24px] font-medium">
+            Bonny and Clyde Street, Ajao Estate, Lagos
+          </div>
+          <div className="font-inter text-[16px] font-semibold">
+            “0” Reviews
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,14 +6,18 @@ import { useState, useEffect } from "react";
 import CustomButton from "./CustomButton";
 import { UserAuth } from "../context/AuthContext";
 import { IoMdExit } from "react-icons/io";
+import { CiSearch } from "react-icons/ci";
+import { useRouter } from "next/navigation";
 
 const NavBar = () => {
   const { user, logOut } = UserAuth();
   const [loading, setLoading] = useState(true);
+  const router = useRouter();
 
   const handleSignOut = async () => {
     try {
       await logOut();
+      router.push("/");
     } catch (error) {
       console.log(error);
     }
@@ -27,8 +31,8 @@ const NavBar = () => {
     checkAuthentication();
   }, [user]);
   return (
-    <header className="w-full bg-white  fixed z-10">
-      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent">
+    <header className="w-full bg-[#F2F6FD]  fixed z-10">
+      <nav className="w-[90%] mx-auto flex justify-between items-center py-4 bg-transparent">
         <Link href="/" className="flex justify-center items-center">
           <Image
             src="/logo.svg"
@@ -43,31 +47,41 @@ const NavBar = () => {
             <CustomButton
               title="LOGIN"
               btnType="button"
-              containerStyles="text-primary-blue rounded-full bg-white min-w-[130px] font-inter font-semibold text-[14px]"
+              containerStyles="text-primary-blue min-w-[130px] font-inter font-semibold text-[14px]"
             />
           </Link>
         ) : (
-          <div className="flex gap-1 font-inter text-16px font-medium items-center justify-between">
-            <p>Welcome</p>
-            <div className="flex flex-col items-end justify-end ml-4">
-              <img
-                src="james.svg"
-                alt="logo"
-                width={20}
-                height={18}
-                className="object-contain"
+          <div className="flex items-center justify-between w-[75%] h-14 ">
+            <div className="flex w-[60%] items-center justify-start gap 2 bg-[#ffffff] p-2 rounded-[10px] border-2 border-gray">
+              <CiSearch />
+              <input
+                type="text"
+                placeholder="Search"
+                className="w-full pl-3 bg-[#F3F7FE] rounded outline-none text-[#101012] text-[14px] font-inter font-normal placeholder-gray-500 border-none bg-[#ffffff]"
               />
-              {/* <p className="text-[14px]">{user.displayName}</p> */}
             </div>
-            <IoMdExit
-              size={25}
-              color="red"
-              className="ml-4 cursor-pointer"
-              onClick={handleSignOut}
-            />
-            {/* <p className="cursor-pointer" onClick={handleSignOut}>
+            <div className="flex gap-1 font-inter text-16px font-medium items-center justify-between">
+              <p>Welcome</p>
+              <div className="flex flex-col items-end justify-end ml-4">
+                <img
+                  src="james.svg"
+                  alt="logo"
+                  width={20}
+                  height={18}
+                  className="object-contain"
+                />
+                {/* <p className="text-[14px]">{user.displayName}</p> */}
+              </div>
+              <IoMdExit
+                size={25}
+                color="red"
+                className="ml-4 cursor-pointer"
+                onClick={handleSignOut}
+              />
+              {/* <p className="cursor-pointer" onClick={handleSignOut}>
               Sign out
             </p> */}
+            </div>
           </div>
         )}
       </nav>
