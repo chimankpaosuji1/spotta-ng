dce5f731cf229892f1c2c2d06221f2f07202d8b7
Chimankpa Osuji <chimankpaosuji@gmail.com>
2024-04-09 01:07:23 +0100

    first commit

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { Places } from "../../contants";
+import { LiaLessThanSolid, LiaGreaterThanSolid } from "react-icons/lia";
+import Image from "next/image";
+import { CustomButton } from "../../components";
 
 const page = () => {
   return (
@@ -11,8 +15,38 @@ const page = () => {
           <div className="font-inter text-[16px] font-semibold">
             “0” Reviews
           </div>
-          <div></div>
+          <ul className="flex items-center justify-center gap-2 overflow-y-hidden xl:overflow-x-hidden overflow-x-scroll xl:w-full w-[50%]">
+            <LiaLessThanSolid />
+            {Places.map((item, index) => (
+              <li
+                key={index}
+                className="flex items-center justify-center gap-[1px] my-[30px] py-0.5 px-1.5 border-2 border-gray-200"
+              >
+                <span className="font-inter text-[14px] font-normal">
+                  {item.name}
+                </span>
+              </li>
+            ))}
+            <LiaGreaterThanSolid />
+          </ul>
         </div>
+      </div>
+      <div className="w-[90%] mx-auto mt-11 p-auto flex flex-col gap-10 items-center justify-center">
+        <Image
+          src="empty.svg"
+          alt="empty"
+          width={323.4}
+          height={255}
+          className="object-contain"
+        />
+        <span className="font-inter text-[16px] font-normal">
+          Oops! No reviews yet.
+        </span>
+        <CustomButton
+          title="LEAVE A REVIEW"
+          btnType="button"
+          containerStyles="bg-[#3366FF] text-white w-[210px] font-inter font-semibold text-[14px] rounded-[6px]"
+        />
       </div>
     </div>
   );
--- contants/index.js
+++ contants/index.js
@@ -56,3 +56,57 @@ export const ReviewSec = [
     buttonStyle: "border-[#A07C22] bg-[#F5E9CB]",
   },
 ];
+
+
+export const Places = [
+  {
+    name: "schools",
+  },
+  {
+    name: "Hospitals",
+  },
+  {
+    name: "Resort Parks",
+  },
+  {
+    name: "Shopping Malls",
+  },
+  {
+    name: "Airpot",
+  },
+  {
+    name: "Train Station",
+  },
+  {
+    name: "Nightlife",
+  },
+  {
+    name: "Public Wifi",
+  },
+  {
+    name: "Parking Lot",
+  },
+  {
+    name: "Security",
+  },
+  {
+    name: "Public Transport",
+  },
+  {
+    name: "Bus Station",
+  },
+  {
+    name: "Quiet",
+  },
+];
+
+export const Reviews = [
+  {
+    name: "James T.",
+    time: "5 months ago",
+    comment: "There is no stable electricity. The roads are fairly good and there is a sense of community. The drainage system is poor and most residents litter their surroundings. There are several grocery stores and Supermarkets.",
+    like: "1224",
+    dislike: "4",
+    message: "24",
+  },
+]
