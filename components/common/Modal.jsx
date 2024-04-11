93f5275756ca59bfea99dab0460b939bc1eb7e92
Chimankpa Osuji <chimankpaosuji@gmail.com>
2024-04-11 11:51:07 +0100

    first commit

--- components/common/Modal.jsx
+++ components/common/Modal.jsx
@@ -12,27 +12,27 @@ const Modal = ({ isOpen, onClose }) => {
   return (
     <div className={`modal ${isOpen ? "block" : "hidden"}`}>
       <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center">
-        <div className="p-8 border w-96 shadow-lg rounded-md bg-white">
-          <div className="text-center">
-            <h3 className="text-2xl font-bold text-gray-900">
+        <div className="p-4 border w-[695px] shadow-lg rounded-md bg-white">
+          <div className="">
+            <h3 className=" text-center text-[18px] font-medium text-[#1E1E1E]">
               Review Location
             </h3>
             <div className="mt-2 px-7 py-3">
-              <p className="text-lg text-gray-500">
+              <p className="text-[20px] font-medium text-[#1E1E1E]">
                 Bonny and Clyde Street, Ajao Estate, Lagos
               </p>
             </div>
-            <div className="flex items-center justify-between mt-4">
+            <div className="flex items-center gap-4 justify-between mt-4">
               {/* Using useRouter to dismiss modal*/}
               <CustomButton
                 title="SUBMIT"
                 handleClick={handleSubmit}
-                containerStyles="px-4 py-2 bg-blue-500 text-white text-base font-medium rounded-md shadow-sm hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300"
+                containerStyles="px-8 py-3 w-full bg-gray-400 text-white text-base font-medium rounded-md shadow-sm hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-gray-300"
               />
               <CustomButton
                 title="CANCEL"
                 handleClick={onClose}
-                containerStyles="px-4 py-2 bg-white border-2 border-blue text-white text-base font-medium rounded-md shadow-sm hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300"
+                containerStyles="px-8 py-3 w-full bg-white border-2 border-blue text-blue text-base font-medium rounded-md shadow-sm hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-300"
               />
             </div>
           </div>
